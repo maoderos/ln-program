ec7fc04e5c80cd5ce42cd9e3abfb05a27e935822
Marcos Derós <marcos@marcosderos.marcosderos.com.br>
2019-01-06 05:56:48 -0200

    bug fix

--- ln-program.c
+++ ln-program.c
@@ -4,10 +4,10 @@
 int main () {
   float x, n;
   printf("Calculating ln(1 + x) by taylor's expansion\n choose de x(it must be less than 1) value and the n(How many expansions and it must be positive)\nx: ");
-  scanf("%f\n", &x);
-  printf("n: ");   //Algum bug por aqui...
-  scanf("%f\n", &n);
-  printf("%f\n", x);
+  scanf("%f", &x);
+  printf("\nn: ");   //Algum bug por aqui...
+  scanf("%f", &n);
+  printf("\n%f", x);
   //verify if the numbers are correct
   if ((n - (int)n) == 0) {
     if (n >= 0 && fabs(x) < 1) {
